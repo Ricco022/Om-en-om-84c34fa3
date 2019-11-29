98fa3357fec3820c47a12a9201c2040fb571b5f8
Riccardo van der Horst <riccardo271002@gmail.com>
2019-11-29 12:24:02 +0100

    uitwerking

---
+++ schaakbord.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Schaakbord</title>
+    <style>
+        td {
+            width: 50px;
+            height: 50px;
+        }
+
+        .jackie {
+            background-color: black;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <tbody>
+            <?php
+                $rows = 8;
+                $collums = 8;
+
+                for ($i=0; $i < $rows ; $i++) { 
+                    if($i%2 === 1){
+                        echo('<tr>');
+                            for ($j=0; $j < $collums; $j++) { 
+                                if($j%2 === 0){
+                                    echo('<td class="jackie"></td>');
+                                } else {
+                                    echo('<td></td>');
+                                }
+                            }
+                        echo('</tr>');
+                    } else {
+                        echo('<tr>');
+                            for ($j=0; $j < $collums; $j++) { 
+                                if($j%2 === 1){
+                                    echo('<td class="jackie"></td>');
+                                } else {
+                                    echo('<td></td>');
+                                }
+                            }
+                        echo('</tr>');
+                    }
+                }
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
